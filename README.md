b1aeab6745245fce741428c1045396d1ad4af14f
gurworla <rlawogur0533@naver.com>
2023-10-14 18:50:30 +0900

    Debounce hook 사용

--- README.md
+++ README.md
@@ -17,3 +17,11 @@ axios 모듈 설치 - npm i axios --save
 설치 - npm i --save styled-components
 
 vscode-styled-components 확장프로그램 설치하여 가독성을 높인다
+
+# React-Router-Dom
+
+react-router-dom 라이브러리를 사용하여 여러 페이지를 SPA로 구성한다.
+
+# Debounce Hooks
+
+Debounce Hook를 만들어 영화 검색시 불필요한 요청을 줄여 성능을 향상시킨다.
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -2,6 +2,7 @@ import axios from "../../api/axios";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./SearchPage.css";
+import { useDebounce } from "../../hooks/useDebounce";
 
 const SearchPage = () => {
   const [searchResult, setSearchResult] = useState("");
@@ -12,12 +13,13 @@ const SearchPage = () => {
 
   let query = useQuery();
   const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   useEffect(() => {
-    if (searchTerm) {
-      fetchSearchMovie(searchTerm);
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [searchTerm]);
+  }, [debouncedSearchTerm]);
 
   const fetchSearchMovie = async () => {
     try {
